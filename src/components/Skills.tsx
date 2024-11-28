0b4a0ca006e6a6c4076f2b904a03ef93b3ec87fe
umair <umairshaikh230934@gmail.com>
2024-11-28 00:13:52 -0800

    first

---
+++ src/components/Skills.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const skills = () => {
+    return (
+        <div id='skills' className='container pt-32'>
+            <div className='grid md:grid-cols-2 gap-20 items-center'>
+            <div data-aos="zoom-in-up">
+                <h2 className='text-4xl md:text-5xl'>Technologies I Work With</h2>
+                <p className='text-gray-500 pt-2'>
+                I have a solid foundation in web development, specializing in HTML, CSS and typescript. My experiance extend to using framework like React and Next.Js to create dynamic and user-friendly applications. Im also proficient in tailwind CSS for efficient styling and design. With a passion for learning, i stay updated on the latest technoligies to enhance my skill set and contribute effectively to projects. 
+                </p>
+            </div>
+            <div>
+                <div className='grid grid-cols-2 text-accent text-3xl sm:text-4xl'>
+                    <div className='space-y-2'>
+                        <h2 data-aos="zoom-in-up">Typescript</h2>
+                        <h2 data-aos="zoom-in-up">React.js</h2>
+                        <h2 data-aos="zoom-in-up">Next.js</h2>
+                         </div>
+                         <div className='space-y-2'>
+                        <h2 data-aos="zoom-in-up">Tailwind</h2>
+                        <h2 data-aos="zoom-in-up">CSS</h2>
+                        <h2 data-aos="zoom-in-up">Node.js</h2>
+                         </div>
+                </div>
+            </div>
+            </div>
+
+        </div>
+    )
+    
+}
+
+ export default skills
